27c42a74ed5f2288cd5c6d5dcc964317a98d6d74
lavanshugarg4 <lavanshugarg45@gmail.com>
2024-02-15 21:39:13 +0530

    commited

---
+++ index.js
@@ -0,0 +1,10 @@
+const figlet = require("figlet");
+
+figlet("Lavanshu Garg", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
